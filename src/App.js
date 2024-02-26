8a1726b3b5fbcccf5267824c8510a3213c5252ee
DewiKimC <97511825+DewiKimC@users.noreply.github.com>
2024-02-26 15:35:50 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import './index.css';
+import './output.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="bg-snow min-h-screen">
+            <div>
+                <nav className="mt-12">
+                    <div className="flex justify-between items-center ">
+                        <p className="ml-40">D.WI</p>
+                        <ul className= "flex mr-40 space-x-8">
+                            <li><a href="#">About me</a></li>
+                            <li><a href="#">Projects</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
+                </nav>
+                <div className="flex flex-col items-center ">
+                    <div className="mt-36 text-center">
+                        <h1>D.WI</h1>
+                        <h2>portfolio of Dewi Berkhof</h2>
+                    </div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+
+    );
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+//   t@type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+      'sans': ['IBM Plex Mono', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
